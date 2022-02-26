e88233ff7560f3125153c872356bff1db77f527f
mlinker2231 <mlinker2231@stu.d214.org>
2022-02-25 23:56:12 -0600

    fcv
    vc

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -28,7 +28,7 @@ public class EnemySpawner : MonoBehaviour
         _numberOfEnemies += level;
         for (int x = 0; x <= _numberOfEnemies; x++)
         {
-            yield return new WaitForSeconds(1.5f - ((float)level / 10));
+            yield return new WaitForSeconds(1.5f - ((float)level / 7));
             var spawnedEnemy = Instantiate(_enemyPrefab, new Vector3(0, _height, -1), Quaternion.identity);
             spawnedEnemy.name = $"Enemy {x}";
             spawnedEnemy.transform.Translate(new Vector3(0, 0));
