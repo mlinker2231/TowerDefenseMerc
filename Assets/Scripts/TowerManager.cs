65c72ad1feaf15920efe3e948d12300a06781c73
mlinker2231 <43888148+mlinker2231@users.noreply.github.com>
2022-03-02 13:31:13 -0600

    zomygod
    sd

---
+++ Assets/Scripts/TowerManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerManager : MonoBehaviour
+{
+
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void SelectBasic()
+    {
+        print("helloee");
+
+    }
+    public void SelectSniper()
+    {
+
+    }
+    public void SelectElectric()
+    {
+
+    }
+    public void SelectBomb()
+    {
+
+    }
+}
