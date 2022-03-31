64f144085ea9c93585850953058b4eb45e1563d6
mlinker2231 <43888148+mlinker2231@users.noreply.github.com>
2022-03-31 13:34:06 -0500

    r
    r

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -26,23 +26,29 @@ public class EnemySpawner : MonoBehaviour
 
     IEnumerator SpawnEnemies()
     {
+
         spawningEnemies = true;
         _numberOfEnemies += level;
         for (int x = 0; x <= _numberOfEnemies; x++)
         {
-            double secondsWaited = ((1/(2* Mathf.Pow(level + 1,5))) * (7 * Mathf.Pow(level + 1,3.8f)));
+           double secondsWaited = ((1 / (2 * Mathf.Pow(level + 1, 5))) * (7 * Mathf.Pow(level + 1, 3.8f)));
             yield return new WaitForSeconds(((float)secondsWaited));
-            var spawnedEnemy = Instantiate(_enemyPrefab, new Vector3(0, _height, -1), Quaternion.identity);
-            spawnedEnemy.name = $"Enemy {x}";
-            spawnedEnemy.transform.Translate(new Vector3(0, 0));
-            spawnedEnemy.health += level / 2;
-            spawnedEnemy._speed += (float)level / 5;                      
-            enemyList.Add(spawnedEnemy);
-
+            SpawnNormalEnemy(x);
         }
         spawningEnemies = false;
         
     }
+
+    private void SpawnNormalEnemy(int x)
+    {
+        var spawnedEnemy = Instantiate(_enemyPrefab, new Vector3(0, _height, -1), Quaternion.identity);
+        spawnedEnemy.name = $"Enemy {x}";
+        spawnedEnemy.transform.Translate(new Vector3(0, 0));
+        spawnedEnemy.health += level / 2;
+        spawnedEnemy._speed += (float)level / 5;
+        enemyList.Add(spawnedEnemy);
+    }
+
     private void CheckForEnemies()
     {
         if (!spawningEnemies && enemyList.Count == 0)
@@ -52,6 +58,7 @@ public class EnemySpawner : MonoBehaviour
         }
 
     }
+
 
 
 }
