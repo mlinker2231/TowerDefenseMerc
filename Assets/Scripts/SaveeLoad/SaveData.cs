d79d5ddafbd18a299a3e0126aaf91b32ba8a425e
mlinker2231 <43888148+mlinker2231@users.noreply.github.com>
2022-04-20 09:44:42 -0500

    halfway

---
+++ Assets/Scripts/SaveeLoad/SaveData.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public static class SaveData
+{
+   public static void SavePlayer (int level)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + "/Users/mikeyLinker/Desktop/Savedthings";
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        PlayerData data = new PlayerData(level);
+
+        formatter.Serialize(stream, data);
+        stream.Close();
+    }
+
+    public static PlayerData LoadPlayer ()
+    {
+        string path = Application.persistentDataPath + "/Users/mikeyLinker/Desktop/Savedthings";
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            PlayerData data = formatter.Deserialize(stream) as PlayerData;
+            stream.Close();
+
+            return data;
+        }
+        else
+        {
+            return null;
+
+        }
+    }
+}
